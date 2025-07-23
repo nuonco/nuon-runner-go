93f192e795d272b28808495e8b8279f0a419157f
Somesh Koli <kolisomesh27@gmail.com>
2025-07-23 22:20:07 +0530

    chore: patch diverged changes

--- client/operations/operations_client.go
+++ client/operations/operations_client.go
@@ -62,6 +62,8 @@ type ClientService interface {
 
 	GetInstallActionWorkflowRun(params *GetInstallActionWorkflowRunParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*GetInstallActionWorkflowRunOK, error)
 
+	GetInstallComponenetLastActivePlan(params *GetInstallComponenetLastActivePlanParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*GetInstallComponenetLastActivePlanOK, error)
+
 	GetRunner(params *GetRunnerParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*GetRunnerOK, error)
 
 	GetRunnerAppConfig(params *GetRunnerAppConfigParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*GetRunnerAppConfigOK, error)
@@ -756,6 +758,45 @@ func (a *Client) GetInstallActionWorkflowRun(params *GetInstallActionWorkflowRun
 	// unexpected success response
 	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
 	msg := fmt.Sprintf("unexpected success response for GetInstallActionWorkflowRun: API contract not enforced by server. Client expected to get an error, but got: %T", result)
+	panic(msg)
+}
+
+/*
+GetInstallComponenetLastActivePlan gets an install component s previous config
+*/
+func (a *Client) GetInstallComponenetLastActivePlan(params *GetInstallComponenetLastActivePlanParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*GetInstallComponenetLastActivePlanOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewGetInstallComponenetLastActivePlanParams()
+	}
+	op := &runtime.ClientOperation{
+		ID:                 "GetInstallComponenetLastActivePlan",
+		Method:             "GET",
+		PathPattern:        "/v1/installs/{install_id}/{component_id}/last-active-plan",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"http"},
+		Params:             params,
+		Reader:             &GetInstallComponenetLastActivePlanReader{formats: a.formats},
+		AuthInfo:           authInfo,
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	}
+	for _, opt := range opts {
+		opt(op)
+	}
+
+	result, err := a.transport.Submit(op)
+	if err != nil {
+		return nil, err
+	}
+	success, ok := result.(*GetInstallComponenetLastActivePlanOK)
+	if ok {
+		return success, nil
+	}
+	// unexpected success response
+	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
+	msg := fmt.Sprintf("unexpected success response for GetInstallComponenetLastActivePlan: API contract not enforced by server. Client expected to get an error, but got: %T", result)
 	panic(msg)
 }
 
