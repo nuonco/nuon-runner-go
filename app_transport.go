72deb39c683e9ae5983a35492686c0c400a957d9
Robert Bruce <alphavibe@gmail.com>
2025-11-20 11:43:31 -0800

    feat: runenr client version

--- app_transport.go
+++ app_transport.go
@@ -1,13 +1,15 @@
 package nuonrunner
 
 import (
+	"fmt"
 	"net/http"
 )
 
 // appTransport is a transport that injects our authentication token and org id into the api request
 type appTransport struct {
-	authToken string
-	orgID     string
+	authToken     string
+	orgID         string
+	clientVersion string
 
 	transport http.RoundTripper
 }
@@ -17,10 +19,17 @@ func (t *appTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	if t.orgID != "" {
 		req.Header.Set("X-Nuon-Org-ID", t.orgID)
 	}
+	if t.clientVersion != "" {
+		req.Header.Set("X-Nuon-Client-Version", t.clientVersion)
+	}
 
 	return t.transport.RoundTrip(req)
 }
 
 func (c *client) SetOrgID(orgID string) {
 	c.appTransport.orgID = orgID
+}
+
+func (c *client) SetClientVersion(version string) {
+	c.appTransport.clientVersion = fmt.Sprintf("runner:%s", version)
 }
